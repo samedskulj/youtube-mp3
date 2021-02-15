e57946c1d2c518c08e2322cceb62eb3f3f038f7d
Samed <samed.skulj08@gmail.com>
2021-02-15 22:20:06 +0100

    Promjena api-a

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ const uzimanjeLinkova = document.getElementById("link");
 const pisanjeLinka = document.getElementById("fname");
 
 function linkovi(videoID) {
-    videoID = pisanjeLinka.value;
-    uzimanjeLinkova.src = `https://www.yt-download.org/api/button/mp3/${videoID}`;
-    uzimanjeLinkova.width = "100%";
-    uzimanjeLinkova.height = "100%";
-   
+  videoID = pisanjeLinka.value;
+  uzimanjeLinkova.src = `https://www.yt2mp3s.me/@api/button/mp3/${videoID}`;
+  uzimanjeLinkova.width = "100%";
+  uzimanjeLinkova.height = "100%";
 }
 
 dugme.addEventListener("click", linkovi);
