869f80f703daa69a4d797236e3c65feee2282152
Samed <samed.skulj08@gmail.com>
2021-03-18 18:42:08 +0100

    Pravi link - implementacija

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const pisanjeLinka = document.getElementById("fname");
 
 function linkovi(videoID) {
   videoID = pisanjeLinka.value;
-  uzimanjeLinkova.src = `https://www.yt2mp3s.me/@api/button/mp3/${videoID}`;
+  let ID = videoID.replace("https://youtu.be/", " ");
+  uzimanjeLinkova.src = `https://www.yt2mp3s.me/@api/button/mp3/${ID}`;
+  console.log(ID);
   uzimanjeLinkova.width = "100%";
   uzimanjeLinkova.height = "100%";
 }
